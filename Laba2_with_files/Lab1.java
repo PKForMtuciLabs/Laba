e67d80186fd91b0e124fbdf2cef0b6027fe94cab
PKForMtuciLabs <kshuterov@mail.ru>
2018-10-08 23:48:10 +0300

    v1

--- Laba2_with_files/Lab1.java
+++ Laba2_with_files/Lab1.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.io.*;
 // Класс Lab1,содержащий методы для ввода координат, проверки различия координат 
 // трех точек, вычисления периметра и площади треугольника.
 public class Lab1{
@@ -6,29 +7,20 @@ public class Lab1{
 // проверяется различие координат трех точек и в зависимости от
 // результата проверки выводится площадь треугольника или сообщением о том,
 // что есть точки с одинаковыми координатами.
-	public static void main(String args[]) {
+	public static void main(String args[]) throws Exception {
+		Double[] ar = readF();
 		System.out.println("Enter x1,y1,z1");
-		Point3d object1 = inFunctionNum();
+		Point3d object1 = new Point3d(ar[0],ar[1],ar[2]);
 		System.out.println("Enter x2,y2,z2");
-		Point3d object2 = inFunctionNum();
+		Point3d object2 = new Point3d(ar[3],ar[4],ar[5]);
 		System.out.println("Enter x3,y3,z3");
-		Point3d object3 = inFunctionNum();
+		Point3d object3 = new Point3d(ar[6],ar[7],ar[8]);
 		if (checkCoords(object1, object2, object3)) {
 			double p = perimetr(object1,object2,object3);
-			System.out.println(square(p, object1, object2, object3));
+			System.out.println(square(p,object1, object2, object3));
 		}
 		else 
 			System.out.println("Coords eq");
-	}
-
-// Метод inFunction получает координаты с командной строки и возвращает созданную точку
-// с указанными координатами.
-	public static Point3d inFunctionNum() {
-		Scanner in = new Scanner(System.in);
-		double x = in.nextDouble();
-		double y = in.nextDouble();
-		double z = in.nextDouble();
-		return new Point3d(x,y,z);
 	}
 
 
@@ -55,6 +47,40 @@ public class Lab1{
 // В качестве параметров передаются 3 точки и периметр.
 // Возвращаемое значение - площадь треугольника.
 	public static double square(double p, Point3d obj1, Point3d obj2, Point3d obj3) {
-		return Math.sqrt(p*(p-obj1.distanceTo(obj2))*(p-obj2.distanceTo(obj3))*(p-obj3.distanceTo(obj2)));
+		double s = Math.sqrt(p*(p-obj1.distanceTo(obj2))*(p-obj2.distanceTo(obj3))*(p-obj3.distanceTo(obj2)));
+		String str = Double.toString(s);
+		writeF(str);
+		return s;
 	}
+// Метод readF осуществляет считывание координат из файла.
+// Возвращаемое значение - массив, содержащий координаты 3-х точек.
+	public static Double[] readF() {
+		Double[] crds = new Double[9];
+		int i = 0;
+		try(FileReader rd = new FileReader("Coords.txt"))
+		{
+			Scanner in = new Scanner(rd);
+			BufferedReader rader = null;
+			while (in.hasNextLine()) {
+				crds[i] = new Double(in.nextLine());
+				i++;
+			}
+			rd.close();
+		}
+		catch(IOException ex) {
+			System.out.println(ex.getMessage());
+		}
+		return crds;
+	}
+// Метод writeF осуществляет запись строки (передается в качестве параметра) в файл
+	public static void writeF(String result) {
+		try(FileWriter wr = new FileWriter("CoordsResult.txt"))
+		{
+			wr.write("Square= " + result);
+			wr.close();
+		}
+		catch(IOException ex) {
+			System.out.println(ex.getMessage());
+		}
+	} 
 }
